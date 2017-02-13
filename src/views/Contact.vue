04e6557ccb82acf10a27bee99e427cb1ea443735
TECNOSYLVA\fgarcia <fernando.garcia@xeridia.com>
2017-02-13 08:53:32 +0100

    Added formspree ajax to contact form. Lacking captcha.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,11 +1,15 @@
 <template lang="pug">
   #contact
+    md-snackbar(ref="snackbar", md-duration="3000", md-position="top center")
+      span {{ alert }}
+      md-button.md-accent(v-on:click.native="close()")
+        md-icon done
     md-card
       md-card-header.center
         .md-title Contact
         .md-subhead Get in touch
       md-card-content
-        form(@submit.stop.prevent="submit")
+        form(v-on:submit.prevent="send()")
           md-input-container
             md-icon.md-primary mail
             label Email
@@ -16,25 +20,50 @@
             md-input(type="text", v-model="subject", required)
           md-input-container
             md-icon.md-primary subject
-            label Content
-            md-textarea(v-model="content", required)
+            label Message
+            md-textarea(v-model="message", required)
           .center
             md-button.md-raised.md-primary(type="submit") Send
             md-button.md-raised(type="reset") Clear
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     data () {
       return {
         email: '',
         subject: '',
-        content: ''
+        message: '',
+        alert: ''
       }
     },
     methods: {
       send () {
-        window.alert('hola')
+        axios.post('https://formspree.io/fergardi@gmail.com', {
+          email: this.email,
+          subject: this.subject,
+          message: this.message
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          responseType: 'json'
+        })
+        .then(() => {
+          this.alert = 'Your email has been sent correctly! I\'ll answer ASAP.'
+          this.open()
+        })
+        .catch(() => {
+          this.alert = 'There were errors sending the email! Please try again.'
+          this.open()
+        })
+      },
+      open () {
+        this.$refs.snackbar.open()
+      },
+      close () {
+        this.$refs.snackbar.close()
       }
     }
   }
